e16c2c0a8982cafe205f4971a9d6203850f0accc
Maximate <otto.kapanen@gmail.com>
2017-05-18 10:40:48 +0300

    Update config.php

--- config.php
+++ config.php
@@ -5,11 +5,11 @@ $password = "password";
 $dbname = "T2017051004";
 
 // Luodaan yhteys
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 // Tarkistetaan että yhteys toimii
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+//if ($conn->connect_error) {
+//    die("Connection failed: " . $conn->connect_error);
+//}
 //Määritetään käytettäväksi UTF-8 merkistöä tietokannan tiedoille
 mysqli_set_charset($conn,"utf8");
 ?>
